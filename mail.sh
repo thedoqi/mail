fcf9b6bb96e0b522acf32a9588b61c25698bcd21
thedoqi <doganc.yuksel@gmail.com>
2020-08-20 16:03:49 +0300

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -4,10 +4,10 @@ hostname=`hostname`
 echo -e "# file: $file \n# hostname: $hostname \n\n"
 rm -f $file.*
 # mailleri listeleme
-cat /var/log/exim_mainlog |grep -E "$(date +"%d %H":)" |grep '=>' |grep -E "outsmtp|queued" |awk '{print $5,$6}' |grep -vE "google|gmail|bounce|${hostname}" | sed 's/<//g;s/>//g;s/(//g;s/)//g;s/,//g;s/ R=dkim_lookuphost//g;s/ R=lookuphost//g;s/ /\n/g' > ${file}.maillist
+cat /var/log/exim_mainlog |grep -E "$(date +"%d %H":)" |grep '=>' |grep -E "outsmtp|queued" |awk '{print $5,$6}' |grep -vE "google|gmail|bounce|${hostname}" | sed 's/<//g;s/>//g;s/(//g;s/)//g;s/,//g;s/R=dkim_lookuphost//g;s/R=lookuphost//g;s/ /\n/g' > ${file}.maillist
 # cat ${file}.maillist
 # tekil domainleri alma
-cat /var/log/exim_mainlog |grep -E "$(date +"%d %H":)" |grep '=>' |grep -E "outsmtp|queued" |awk '{print $5,$6}' |grep -vE "google|gmail|bounce|${hostname}" | sed 's/<//g;s/>//g;s/(//g;s/)//g;s/,//g;s/ R=dkim_lookuphost//g;s/ /\n/g' | sed 's/@/ /g' |awk {'print $2'} |s$
+cat /var/log/exim_mainlog |grep -E "$(date +"%d %H":)" |grep '=>' |grep -E "outsmtp|queued" |awk '{print $5,$6}' |grep -vE "google|gmail|bounce|${hostname}" | sed 's/<//g;s/>//g;s/(//g;s/)//g;s/,//g;s/R=dkim_lookuphost//g;s/ /\n/g' | sed 's/@/ /g' |awk {'print $2'} |sort -n |uniq |grep "." > ${file}.domain
 # cat ${file}.domain
 while read domain
 do
